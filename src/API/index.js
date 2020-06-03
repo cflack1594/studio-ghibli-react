b9f1e7a81d54f000d4a695f40fe3cf639faf9bff
cflack1594 <cflack1594@gmail.com>
2020-06-03 17:32:56 -0500

    ADD error handling to API fetch function

--- src/API/index.js
+++ src/API/index.js
@@ -1,4 +1,10 @@
 export const getStudioGhibliData = async (target) => {
-  const resp = await window.fetch(`https://ghibliapi.herokuapp.com/${target}`);
-  return resp.json();
+  try {
+    const resp = await window.fetch(
+      `https://ghibliapi.herokuapp.com/${target}`
+    );
+    return resp.json();
+  } catch (error) {
+    throw new Error("Failed to fetch");
+  }
 };
